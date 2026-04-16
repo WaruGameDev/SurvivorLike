7f8cfc8c0ae8476c0c9156ba6ca24c10a379b729
WaruGameDev <60048774+WaruGameDev@users.noreply.github.com>
2026-04-16 14:11:14 -0400

    katana y escema

--- Assets/01_Script/03_Weapons/Katana.cs
+++ Assets/01_Script/03_Weapons/Katana.cs
@@ -7,8 +7,9 @@ public class Katana : WeaponBase
 
     public override void Shot()
     {
-        Vector2 direction = PlayerManager.instance.actualInput.lastDirection;
-        KatanaProjectile projectile = Instantiate(katanaProjectile, transform.position, Quaternion.Euler(direction));
+        Vector2 direction = PlayerManager.instance.actualInput.lastDirection;      
+
+        KatanaProjectile projectile = Instantiate(katanaProjectile, transform.position,Quaternion.Euler(direction) );
         projectile.direction = direction;
     }
 }
